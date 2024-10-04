01a731901b33a36a27acad9c3499a495c9e93190
OMMICANG <ommicang@gmail.com>
2024-10-04 06:07:22 +0100

    Touching Telegram UX

--- top/app/page.tsx
+++ top/app/page.tsx
@@ -5,6 +5,7 @@ import Preloader from './preloader/page';
 import LandingPage from './landingPage/page';
 import IsMobile from './components/IsMobile';
 
+// Define the type for the window object with Telegram
 interface TelegramWindow extends Window {
   Telegram: {
     WebApp: {
@@ -14,36 +15,60 @@ interface TelegramWindow extends Window {
   };
 }
 
+// Create a type assertion for window
 declare const window: TelegramWindow;
 
-const Home = () => {
+const Home: React.FC = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    console.log('useEffect triggered');
-    
     const handleTelegramInit = () => {
-      console.log('Telegram WebApp initialization started');
+      const webapp = window.Telegram.WebApp;
 
-      if (window.Telegram && window.Telegram.WebApp) {
-        const webapp = window.Telegram.WebApp;
-        webapp.ready(); 
-        webapp.expand(); 
+      webapp.ready(); // Initialize The Web App
+
+      webapp.expand(); // Expand The Web App To Full Height
+
+
+// interface TelegramWindow extends Window {
+//   Telegram: {
+//     WebApp: {
+//       ready: () => void;
+//       expand: () => void;
+//     };
+//   };
+// }
+
+// declare const window: TelegramWindow;
+
+// const Home = () => {
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     console.log('useEffect triggered');
+    
+//     const handleTelegramInit = () => {
+//       console.log('Telegram WebApp initialization started');
+
+//       if (window.Telegram && window.Telegram.WebApp) {
+//         const webapp = window.Telegram.WebApp;
+//         webapp.ready(); 
+//         webapp.expand(); 
 
         // Simulate a loading delay
         setTimeout(() => {
           console.log('Preloader finished, showing LandingPage');
           setLoading(false);
         }, 2000);
-      } else {
-        console.log('Telegram WebApp not found');
-      }
+      // } else {
+      //   console.log('Telegram WebApp not found');
+      // }
     };
 
     if (window.Telegram) {
       handleTelegramInit();
     } else {
-      console.log('Listening for TelegramWebAppReady event');
+      // console.log('Listening for TelegramWebAppReady event');
       document.addEventListener('TelegramWebAppReady', handleTelegramInit);
     }
 

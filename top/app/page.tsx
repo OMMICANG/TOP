3ae557df8e82d7db3ec29c0c9411a70102c0ba42
OMMICANG <ommicang@gmail.com>
2024-10-04 07:40:43 +0100

    TG Onload expand() UX fixed

--- top/app/page.tsx
+++ top/app/page.tsx
@@ -31,7 +31,7 @@ const Home = () => {
       // Simulate a loading delay for demonstration purposes
       setTimeout(() => {
         setLoading(false);
-      }, 2000); // Adjust the delay as needed
+      }, 5000); // Adjust the delay as needed
     };
 
     const checkTelegramSDK = () => {

21039ef273993933d71338a56546bf6f6985131f
OMMICANG <ommicang@gmail.com>
2024-10-06 10:45:14 +0100

    breaking kyc phase into components for better code management

---
+++ top/app/kyc/faceCapture/page.tsx
@@ -0,0 +1,8 @@
+// pages/kyc/face-capture.tsx
+import KYCPhase2 from "../kycPhase2/page";
+
+const FaceCapturePage: React.FC = () => {
+  return <KYCPhase2 />;
+};
+
+export default FaceCapturePage;
---
+++ top/app/kyc/kycPhase2/page.tsx
@@ -0,0 +1,18 @@
+// components/KYCPhase2.tsx
+"use client";
+
+import IsMobile from "../../components/IsMobile";
+
+const KYCPhase2: React.FC = () => {
+  return (
+    <IsMobile>
+      <div>
+        <h1>Face Capture - Phase 2</h1>
+        <p>This is where the user will take a picture of their face.</p>
+        {/* You will implement the face capture logic here in the next steps */}
+      </div>
+    </IsMobile>
+  );
+};
+
+export default KYCPhase2;

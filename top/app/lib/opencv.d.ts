1edf5443af5c647a7908eb13fa695a67017bf57e
OMMICANG <ommicang@gmail.com>
2024-12-02 22:52:25 +0100

    <FIX: Type error: Could not find a declaration file for module 'canvas-confetti'. '/vercel/path0/top/node_modules/canvas-confetti/src/confetti.js' implicitly has an 'any' type. ++>

--- top/app/lib/opencv.d.ts
+++ top/app/lib/opencv.d.ts
@@ -2,4 +2,5 @@ declare module 'opencv.js';
 declare module 'react-google-recaptcha';
 declare module 'js-cookie';
 declare module 'bcryptjs';
+declare module 'canvas-confetti';
 // declare module '@ffmpeg/ffmpeg';

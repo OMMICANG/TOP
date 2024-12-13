1e28f33de95b3fd4aadd75d398ecdfb1f5dc5b92
OMMICANG <ommicang@gmail.com>
2024-12-13 18:48:29 +0100

    <FEAT: Extending BackIcon Component to p2pPostAd Page>

--- top/app/spot/p2pPostAd/page.tsx
+++ top/app/spot/p2pPostAd/page.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import BackIcon from "../_components/BackIcon";
 import styles from "../styles/P2pPostAd.module.css";
 
 const P2pPostAd: React.FC = () => {
   return (
-    <div className={styles.container}>
+    <div className={styles.bodyContainer}>
+      <div className={styles.upperIndex}>
+        <div className={styles.topHeaderContainer}>
+          <div className={styles.backIcon}>
+            <BackIcon />
+          </div>
+        </div>
+      </div>
+
+
       <button className={styles.postAdButton}>Post Ad</button>
     </div>
   );
